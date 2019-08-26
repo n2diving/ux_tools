272e3b610e647bfca1e8a882469d3481d5e36788
n2diving <alexis@gormley-design.com>
2019-08-25 23:47:12 -0700

    changing root to empathy map record, fixing table styling

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "empathy_maps#index"
+  root to: "empathy_map_records#index"
 
   # devise_for :users
   # resources :users
